8172a67e7be879cba768fac55168bce19781d361
Yashwanth R <chawatyash@gmail.com>
2020-01-25 00:28:33 +0530

    Filters added

--- filter.cpp
+++ filter.cpp
@@ -53,7 +53,38 @@ namespace filter {
                 h.push_back((1 / (PI * (i - phi))) * (sin((i - phi) * PI / F) - sin((i - phi) * 2 * PI * fc / F)));
             }    
         }
+        return h;
+    }
 
+    vector<complex<float>> FIRBandPass(float f1, float f2, float F, int N, int phi, string window) {
+        vector<float> t;
+        vector<complex<float>> h;
+        t = utils::linspace(-(N - 1) / 2, (N - 1) / 2, N);
+
+        for (float i : t) {
+            
+            if ((i - phi) == 0) {
+                h.push_back(2 * (f2 - f1) / F);
+            } else {
+                h.push_back((1 / (PI * (i - phi))) * (sin((i - phi) * 2 * PI * f2 / F) - sin((i - phi) * 2 * PI * f1 / F)));
+            }    
+        }
+        return h;
+    }
+
+    vector<complex<float>> FIRBandStop(float f1, float f2, float F, int N, int phi, string window) {
+        vector<float> t;
+        vector<complex<float>> h;
+        t = utils::linspace(-(N - 1) / 2, (N - 1) / 2, N);
+
+        for (float i : t) {
+            
+            if ((i - phi) == 0) {
+                h.push_back(1 - 2 * (f1 - f2) / F);
+            } else {
+                h.push_back((1 / (PI * (i - phi))) * (sin((i - phi) * PI / F) + sin((i - phi) * 2 * PI * f1 / F) - sin((i - phi) * 2 * PI * f2 / F)));
+            }    
+        }
         return h;
     }
 }
